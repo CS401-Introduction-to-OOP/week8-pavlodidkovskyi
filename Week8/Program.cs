d0ca238e42570709447bf641876d16475f737388
pavlodidkovskyi <pavlo.didkovskyi.25@kse.org.ua>
2026-03-24 18:49:20 +0200

    1 stage

--- Week8/Program.cs
+++ Week8/Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿public abstract class DeliveryItem
+{
+    public string TrackingNumber { get; private set; }
+    public double Weight { get; private set; }
 
-Console.WriteLine("Hello, World!");
+    public DeliveryItem(string trackingNumber, double weight)
+    {
+        TrackingNumber = trackingNumber;
+        Weight = weight;
+    }
+
+    public abstract double CalculateCost();
+
+    public virtual void PrintInfo()
+    {
+        Console.WriteLine($"Track - {TrackingNumber}, Weight - {Weight} kg (not N)");
+    }
+}
+
+public class Letter : DeliveryItem
+{
+    public Letter(string trackingNumber, double weight) : base(trackingNumber, weight)
+    {
+    }
+
+    public override double CalculateCost()
+    {
+        return 15 + (Weight * 10);
+    }
+}
+
+public class Parcel : DeliveryItem
+{
+    public string Dimensions { get; private set; }
+
+    public Parcel(string trackingNumber, double weight, string dimensions) : base(trackingNumber, weight)
+    {
+        Dimensions = dimensions;
+    }
+
+    public override double CalculateCost()
+    {
+        return 50 + (Weight * 25);
+    }
+
+    public override void PrintInfo()
+    {
+        base.PrintInfo();
+        Console.WriteLine($"Dimensions - {Dimensions}");
+    }
+}
